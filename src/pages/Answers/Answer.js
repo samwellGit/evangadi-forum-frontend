b4f988f5ce8f71026c5955c36932583544b8aaf0
samwellGit <samuluva@yahoo.com>
2024-01-13 18:31:41 -0800

    change made

--- src/pages/Answers/Answer.js
+++ src/pages/Answers/Answer.js
@@ -1,6 +1,3 @@
-
-
-
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -11,22 +8,57 @@ import "./Answer.css";
 const Answer = () => {
   const [form, setForm] = useState({});
   const [answer, setAnswer] = useState([]);
-  const [question, setQuestion] = useState([]); // Corrected the state name
+  const [question, setQuestions] = useState([]);
   const { questionid } = useParams();
   const token = localStorage.getItem("token");
   const axios = axiosBase();
+  //handle change value
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-  // ... (other functions remain unchanged)
+  //handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `answers/postanswer/${questionid}`,
+        form,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      e.target.reset();
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  //loadanswers
+  const loadanswers = async () => {
+    try {
+      const { data } = await axios.get(`/answers/allanswer/${questionid}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      setAnswer(data.answer);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   //loadQuestions
-  const loadQuestions = async () => {
+  const loadQuetions = async () => {
     try {
       const { data } = await axios.get(`questions/question/${questionid}`, {
         headers: {
           Authorization: "Bearer " + token,
         },
       });
-      setQuestion(data); // Corrected the state name
+      setQuestions(data);
     } catch (error) {
       console.log(error.response);
     }
@@ -34,161 +66,60 @@ const Answer = () => {
 
   useEffect(() => {
     loadanswers();
-    loadQuestions();
+    loadQuetions();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [question]);
-
   return (
     <div className="container title-container">
-      {/* ... (other parts of your component) */}
+      <br />
+      <br />
+      <br />
+      <br />
       <div className="titl">
         <h2 className="title">Questions</h2>
 
         <div className="question_titles">
-          {question ? question.title : "newTitle"}
+          {question ? question.title : "newTitle"}4
         </div>
 
         <div className="question_disc">
           {question ? question.description : "newdiscription"}
         </div>
 
-        {/* ... (other parts of your component) */}
+        <h2 className="community_title">Answer From The Community</h2>
+        <div>
+          {answer.map((answe, i) => (
+            <AnswerDetail answers={answe} key={i} />
+          ))}
+        </div>
       </div>
-      {/* ... (other parts of your component) */}
+      <div className="container">
+        <form method="post" onSubmit={handleSubmit}>
+          <div>
+            <textarea
+              style={{
+                marginTop: "15px",
+                height: "150px",
+                width: "100%",
+                borderRadius: "10px",
+                padding: "10px 15px",
+              }}
+              maxLength="200"
+              type="text"
+              name="answer"
+              placeholder="Your Answer . . . "
+              onChange={handleChange}
+            />
+          </div>
+          <div className="btn-answer">
+            <button className="btn btn_answer btn-primary" type="submit">
+              Post Answer
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Answer;
-
-
-
-
-
-
-
-// import React, { useEffect } from "react";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
-// import axiosBase from "../../components/axios";
-// import AnswerDetail from "./AnswerDetail";
-// import "./Answer.css";
-
-// const Answer = () => {
-//   const [form, setForm] = useState({});
-//   const [answer, setAnswer] = useState([]);
-//   const [question, setQuestions] = useState([]);
-//   const { questionid } = useParams();
-//   const token = localStorage.getItem("token");
-//   const axios = axiosBase();
-//   //handle change value
-//   const handleChange = (e) => {
-//     setForm({ ...form, [e.target.name]: e.target.value });
-//   };
-
-//   //handle submit
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await axios.post(
-//         `answers/postanswer/${questionid}`,
-//         form,
-//         {
-//           headers: {
-//             Authorization: "Bearer " + token,
-//           },
-//         }
-//       );
-//       e.target.reset();
-//     } catch (error) {
-//       console.log(error.response);
-//     }
-//   };
-
-//   //loadanswers
-//   const loadanswers = async () => {
-//     try {
-//       const { data } = await axios.get(`/answers/allanswer/${questionid}`, {
-//         headers: {
-//           Authorization: "Bearer " + token,
-//         },
-//       });
-//       setAnswer(data.answer);
-//     } catch (error) {
-//       console.log(error.response);
-//     }
-//   };
-
-//   //loadQuestions
-//   const loadQuetions = async () => {
-//     try {
-//       const { data } = await axios.get(`questions/question/${questionid}`, {
-//         headers: {
-//           Authorization: "Bearer " + token,
-//         },
-//       });
-//       setQuestions(data);
-//     } catch (error) {
-//       console.log(error.response);
-//     }
-//   };
-
-//   useEffect(() => {
-//     loadanswers();
-//     loadQuetions();
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [question]);
-//   return (
-//     <div className="container title-container">
-//       <br />
-//       <br />
-//       <br />
-//       <br />
-//       <div className="titl">
-//         <h2 className="title">Questions</h2>
-
-//         <div className="question_titles">
-//           {question ? question.title : "newTitle"}4
-//         </div>
-
-//         <div className="question_disc">
-//           {question ? question.description : "newdiscription"}
-//         </div>
-
-//         <h2 className="community_title">Answer From The Community</h2>
-//         <div>
-//           {answer.map((answe, i) => (
-//             <AnswerDetail answers={answe} key={i} />
-//           ))}
-//         </div>
-//       </div>
-//       <div className="container">
-//         <form method="post" onSubmit={handleSubmit}>
-//           <div>
-//             <textarea
-//               style={{
-//                 marginTop: "15px",
-//                 height: "150px",
-//                 width: "100%",
-//                 borderRadius: "10px",
-//                 padding: "10px 15px",
-//               }}
-//               maxLength="200"
-//               type="text"
-//               name="answer"
-//               placeholder="Your Answer . . . "
-//               onChange={handleChange}
-//             />
-//           </div>
-//           <div className="btn-answer">
-//             <button className="btn btn_answer btn-primary" type="submit">
-//               Post Answer
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Answer;
